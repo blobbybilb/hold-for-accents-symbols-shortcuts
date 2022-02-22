83f963b314b1766643f65a138f7eee4ebc8fa728
blobbybilb <58201828+blobbybilb@users.noreply.github.com>
2022-02-21 22:50:34 -0800

    save before removing thread and using after()

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import keyboard
 from time import sleep
 import json
 import tkinter
-import faulthandler
-
-faulthandler.enable()
+import threading
 
 
-VALID_KEYS = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./\\"
+VALID_KEYS = "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 KEYS_OPTION_SELECT = "1234567890-="
 
-with open("keys_config.json") as config_file:
+with open("keys_config.json", encoding='utf-8') as config_file:
     CONFIG = json.load(config_file)
     key_delay = round(CONFIG["delay"] / 0.05)
     keys = CONFIG["keys"]
+
+
+def other_run_on_press(key_info: keyboard.KeyboardEvent):
+    if key_info.name in KEYS_OPTION_SELECT:
+        keyboard.write(keys[key_info.name][int(key_info.name)+])
+    elif key_info.name == 'esc':
+        input_win.destroy()
+
+def checker():
+    keyboard.on_press(other_run_on_press)
 
 
 def run_on_press(key_info: keyboard.KeyboardEvent):
@@ -23,18 +31,25 @@ def run_on_press(key_info: keyboard.KeyboardEvent):
         sleep(0.05)
         if not keyboard.is_pressed(key_info.name):
             return
+    keyboard.release(key_info.name)
     keyboard.press_and_release("backspace")
     input_win = tkinter.Tk()
-    input_win.overrideredirect(1)
+    input_win.overrideredirect(True)
     input_win.geometry("500x50")
     input_win.geometry("-50-50")
 
-    for each_item in keys[key_info.name]:
-        tkinter.Label(input_win, text=each_item, width=len(each_item)).pack()
+    items_for_this = keys[key_info.name]
+    for each_item in items_for_this:
+        print(each_item)
+        tkinter.Label(input_win, text=each_item, width=5).pack(side="left")
 
+    input_win.attributes("-topmost", True)
+
+    checker_thread = threading.Thread(target=checker)
     input_win.mainloop()
 
 
 keyboard.on_press(run_on_press)
 
-sleep(10)  # why is this causing segmentation fault?
+
+keyboard.wait()
