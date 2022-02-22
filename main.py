5e8b0d7f0039e8773a503675797dcdac78a07e6f
blobbybilb <58201828+blobbybilb@users.noreply.github.com>
2022-02-22 13:15:43 -0800

    almost final

--- main.py
+++ main.py
@@ -3,30 +3,12 @@ from time import sleep
 import json
 import tkinter
 
-
 VALID_KEYS = "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 KEYS_OPTION_SELECT = "1234567890-="
-
 with open("keys_config.json", encoding='utf-8') as config_file:
     CONFIG = json.load(config_file)
     key_delay = round(CONFIG["delay"] / 0.05)
     keys = CONFIG["keys"]
-
-
-def check_for_press(text_options_list, the_win):
-    while True:
-        for i in KEYS_OPTION_SELECT:
-            if keyboard.is_pressed(i):
-                if i == '0':
-                    keyboard.write(text_options_list[9])
-                elif i == '-':
-                    keyboard.write(text_options_list[10])
-                elif i == '=':
-                    keyboard.write(text_options_list[11])
-                else:
-                    keyboard.write(text_options_list[int(i)-1])
-                the_win.destroy()
-
 
 
 def run_on_press(key_info: keyboard.KeyboardEvent):
@@ -40,20 +22,37 @@ def run_on_press(key_info: keyboard.KeyboardEvent):
     keyboard.press_and_release("backspace")
     input_win = tkinter.Tk()
     input_win.overrideredirect(True)
-    input_win.geometry("500x50")
     input_win.geometry("-50-50")
+    input_win.attributes("-topmost", True)
+    input_win.configure(bg='black')
 
     items_for_this = keys[key_info.name]
-    for each_item in items_for_this:
-        tkinter.Label(input_win, text=each_item, width=5).pack(side="left")
+    for index, each_item in enumerate(items_for_this):
+        tkinter.Label(input_win, text=each_item, width=len(each_item), font=("Helvetica", 20), bg='black', fg='light blue').grid(row=1, column=index)
+        tkinter.Label(input_win, text=index+1, width=5, font=("Helvetica", 10), bg='black', fg='light grey').grid(row=2, column=index)
 
-    input_win.attributes("-topmost", True)
     input_win.update()
-    print('hello')
-    input_win.after(10, check_for_press, items_for_this, input_win)
+    while True:
+        for i in KEYS_OPTION_SELECT:
+            if keyboard.is_pressed(i):
+                if i == '0':
+                    char_select = (items_for_this[9])
+                elif i == '-':
+                    char_select = (items_for_this[10])
+                elif i == '=':
+                    char_select = (items_for_this[11])
+                else:
+                    try:
+                        char_select = (items_for_this[int(i) - 1])
+                    except IndexError:
+                        input_win.destroy()
+                        keyboard.press_and_release('backspace')
+                        return
+                input_win.destroy()
+                keyboard.press_and_release('backspace')
+                keyboard.write(char_select)
+                return
 
 
 keyboard.on_press(run_on_press)
-
-
 keyboard.wait()
